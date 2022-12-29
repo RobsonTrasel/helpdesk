dbdd2bf07975c439f1dc173c085480510723d701
Robson Trasel <robson.trasel@gmail.com>
2022-12-28 22:43:19 -0300

    chore: add router to app.ts

--- src/shared/data/http/app.ts
+++ src/shared/data/http/app.ts
@@ -1,9 +1,11 @@
 import express from 'express'
 import cors from 'cors'
+import { router } from './routes'
 const app = express()
 
 app.use(cors())
 app.use(express.json())
+app.use(router)
 
 
 export { app }
